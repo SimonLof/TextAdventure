0f2e5769b6a98f96239cf31ab75483576bdcd417
SimonLof <lofgren.simon@gmail.com>
2023-10-01 07:23:39 +0200

    Added inputHandler

---
+++ TextAdventure/Classes/InputHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace TextAdventure.Classes
+{
+    public class InputHandler
+    {
+        public string InputString { get; set; }
+
+        public void GetOutcome(ref Player player, ref Map map, ref Room room)
+        {
+
+        }
+    }
+}
