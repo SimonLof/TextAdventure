02346501571d0d5755c4c3330b439a299478a0b4
SimonLof <simon.lofgren@yh.nackademin.se>
2023-10-14 15:48:51 +0200

    Riddle works better with more options. Basic item interaction added, not tested.

---
+++ TextAdventure/Classes/ItemInteraction.cs
@@ -0,0 +1,24 @@
+﻿using TextAdventure.Classes.EffectClasses;
+
+namespace TextAdventure.Classes
+{
+    public class ItemInteraction
+    {
+        public int FirstItemId { get; set; }
+        public int SecondItemId { get; set; }
+        public List<Effect> CombineEffects { get; set; }
+        public ItemInteraction(int firstItemId, int secondItemId, List<Effect> effects)
+        {
+            FirstItemId = firstItemId;
+            SecondItemId = secondItemId;
+            CombineEffects = effects;
+        }
+        public void CombineEffect() 
+        { 
+            foreach(Effect effect in CombineEffects)
+            {
+                effect.DoEffect();
+            }
+        }
+    }
+}
